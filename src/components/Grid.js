dd3464a6276641aa5ee79634b9c2ac8352842250
lucjovi2001 <171295225+lucjovi2001@users.noreply.github.com>
2024-06-12 17:01:13 -0700

    Add row grid to home screen

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Row from './Row.js'
 
-export default function Grid() {
+export default function Grid({ currentGuess, guesses, turn }) {
     return (
-        <div>Grid</div>
+        <div>
+            {guesses.map((g, i) => {
+                return <Row key={i} />
+            })}
+        </div>
     )
 }
