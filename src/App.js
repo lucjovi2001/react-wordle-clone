173bdd38e2ebd7b8e66f7e829a21fd19768deb49
lucjovi2001 <171295225+lucjovi2001@users.noreply.github.com>
2024-06-15 13:25:13 -0700

    Add game over modal and complete app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Wordle (Clone)</h1>
+      <h1>"Wordle" (not really...)</h1>
       {solution && <Wordle solution={solution} />}
     </div>
   );
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,31 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from '../hooks/useWordle'
 import Grid from './Grid.js'
 import Keypad from './Keypad.js'
+import Modal from './Modal.js'
 
 export default function Wordle({ solution }) {
     const { currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys } = useWordle(solution)
+    const [showModal, setShowModal] = useState(false)
 
     useEffect(() => {
         window.addEventListener('keyup', handleKeyup)
 
-        if (isCorrect || turn > 5) {
+        if (isCorrect) {
+            setTimeout(() => setShowModal(true), 2000)
             window.removeEventListener('keyup', handleKeyup)
         }
 
-        // if (turn > 5) {
-        //     window.removeEventListener('keyup', handleKeyup)
-        // }
+        if (turn > 5) {
+            setTimeout(() => setShowModal(true), 2000)
+            window.removeEventListener('keyup', handleKeyup)
+        }
 
         return () => window.removeEventListener('keyup', handleKeyup)
     }, [handleKeyup, isCorrect, turn])
 
     return (
         <div>
-            <div>solution - {solution}</div>
-            <div>current guess - {currentGuess}</div>
             <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
             <Keypad usedKeys={usedKeys} />
+            {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
         </div>
     )
 }
